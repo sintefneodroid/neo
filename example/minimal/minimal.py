207607a4cddace6ea407425d18217277c2ec6d64
Chrstian Heider Nielsen <mrnaah@gmail.com>
2017-12-12 17:31:42 +0100

    Configurations working

--- example/minimal/minimal.py
+++ example/minimal/minimal.py
@@ -5,13 +5,17 @@ from neodroid import Configuration
 
 def main():
 
-  _environment = neo.make('3d_grid_world_win', connect_to_running=False)
+  _environment = neo.make('3d_grid_world_win', connect_to_running=True)
   _environment.seed(42)
 
   for i in range(1000):
 
-    observations, info = _environment.configure([Configuration('PlayerTransformY',i%6),Configuration('PlayerTransformX',-(i%3)),Configuration('PlayerTransformZ',-(i%3))])
-    for j in range(100):
+    observations, info = _environment.configure()
+    x,y,z = info.get_observer(b'GoalObserver').get_position()
+    rot_x,rot_y,rot_z = info.get_observer(b'GoalObserver').get_rotation()
+    observations, info = _environment.configure([Configuration('PlayerTransformX',x),Configuration('PlayerTransformY',y+i%5),Configuration('PlayerTransformZ',z)])
+    _environment.act()
+    for j in range(1000):
       actions = _environment.sample_action_space(binary=True,discrete=True)
       _, reward, interrupted, info = _environment.act(actions)
       if interrupted:
