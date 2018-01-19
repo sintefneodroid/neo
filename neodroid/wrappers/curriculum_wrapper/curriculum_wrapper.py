33a5be0c9ff5007f8907fcd8500108d50112263c
Christian Heider Nielsen <mrnaah@gmail.com>
2018-01-19 20:38:12 +0100

    Curriculum wrapper

--- neodroid/wrappers/curriculum_wrapper/curriculum_wrapper.py
+++ neodroid/wrappers/curriculum_wrapper/curriculum_wrapper.py
@@ -1,7 +1,7 @@
 from neodroid import NeodroidEnvironment
 from neodroid.models import ReactionParameters, Reaction
 from neodroid.utilities.statics import flattened_observation
-
+import numpy as np
 
 class NeodroidCurriculumWrapper(NeodroidEnvironment):
   def __init__(self, *args, **kwargs):
@@ -15,7 +15,7 @@ class NeodroidCurriculumWrapper(NeodroidEnvironment):
   def act(self, *args, **kwargs):
     message = super(NeodroidCurriculumWrapper, self).react(*args, **kwargs)
     if message:
-      return (flattened_observation(message),
+      return (np.array(flattened_observation(message)),
               message.signal,
               message.terminated, message)
     return None,None,None,None
@@ -23,10 +23,10 @@ class NeodroidCurriculumWrapper(NeodroidEnvironment):
   def configure(self, *args, **kwargs):
     message = super(NeodroidCurriculumWrapper, self).reset(*args, **kwargs)
     if message:
-      return flattened_observation(message), message
+      return np.array(flattened_observation(message))
     return None,None
 
-  def generate_initial_states_from_configuration(self, initial_configuration, motion_horizon=3, num=10):
+  def generate_initial_states_from_configuration(self, initial_configuration, motion_horizon=6, num=30):
 
     initial_states = []
     configure_params = ReactionParameters(terminable=False, episode_count=False, reset=True, configure=True)
@@ -43,7 +43,7 @@ class NeodroidCurriculumWrapper(NeodroidEnvironment):
 
     return initial_states
 
-  def generate_initial_states_from_state(self, state, motion_horizon=3, num=10):
+  def generate_initial_states_from_state(self, state, motion_horizon=10, num=100):
     initial_states = []
 
     while len(initial_states) < num:
