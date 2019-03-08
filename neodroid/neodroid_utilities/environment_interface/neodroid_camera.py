83f8515e6280e08d60c85d953067193ef1fd43c4
cnheider <mrnaah@gmail.com>
2019-03-08 17:27:31 +0100

    camera

--- neodroid/neodroid_utilities/environment_interface/neodroid_camera.py
+++ neodroid/neodroid_utilities/environment_interface/neodroid_camera.py
@@ -7,62 +7,62 @@ __author__ = 'cnheider'
 
 
 def extract_neodroid_camera_images(environment):
-  rgb = environment.sensor('RGBCameraObserver')
+  rgb = environment.sensor('RGB')
   rgb_image = None
   if rgb:
     rgb_image = Image.open(rgb.observation_value)
 
-  depth = environment.sensor('DepthCameraObserver')
+  depth = environment.sensor('Depth')
   depth_image = None
   if depth:
     depth_image = Image.open(depth.observation_value)
 
-  segmentation = environment.sensor('SegmentationCameraObserver')
+  segmentation = environment.sensor('Segmentation')
   segmentation_image = None
   if segmentation:
     segmentation_image = Image.open(segmentation.observation_value)
 
-  instance_segmentation = environment.sensor('InstanceSegmentationCameraObserver')
+  instance_segmentation = environment.sensor('InstanceSegmentation')
   instance_segmentation_image = None
   if instance_segmentation:
     instance_segmentation_image = Image.open(instance_segmentation.observation_value)
 
-  infrared = environment.sensor('InfraredShadowCameraObserver')
+  infrared = environment.sensor('InfraredShadow')
   infrared_image = None
   if infrared:
     infrared_image = Image.open(infrared.observation_value)
 
-  flow = environment.sensor('FlowCameraObserver')
+  flow = environment.sensor('Flow')
   flow_image = None
   if flow:
     flow_image = Image.open(flow.observation_value)
 
-  normal = environment.sensor('NormalCameraObserver')
+  normal = environment.sensor('Normal')
   normal_image = None
   if normal:
     normal_image = Image.open(normal.observation_value)
 
-  satellite = environment.sensor('SatelliteCameraObserver')
+  satellite = environment.sensor('Satellite')
   satellite_image = None
   if satellite:
     satellite_image = Image.open(satellite.observation_value)
 
-  object_space = environment.sensor('ObjectSpaceCameraObserver')
+  object_space = environment.sensor('ObjectSpace')
   object_space_image = None
   if object_space:
     object_space_image = Image.open(object_space.observation_value)
 
-  world_space = environment.sensor('WorldSpaceCameraObserver')
+  world_space = environment.sensor('WorldSpace')
   world_space_image = None
   if world_space:
     world_space_image = Image.open(world_space.observation_value)
 
-  uvs = environment.sensor('UVsCameraObserver')
+  uvs = environment.sensor('UVs')
   uvs_image = None
   if uvs:
     uvs_image = Image.open(uvs.observation_value)
 
-  tangents = environment.sensor('TangentsCameraObserver')
+  tangents = environment.sensor('Tangents')
   tangents_image = None
   if tangents:
     tangents_image = Image.open(tangents.observation_value)
