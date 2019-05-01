10f1d3438e06275b4efb99888c34fa8501debc00
Christian Heider Nielsen <mrnaah@gmail.com>
2019-05-01 15:45:30 +0200

    Version

--- NeodroidPackage.py
+++ NeodroidPackage.py
@@ -1,6 +1,11 @@
 from setuptools import find_packages
 
-from neodroid.version import get_version
+import os
+import re
+
+with open(os.path.join(os.path.dirname(__file__), "neo/version.py"), "r") as f:
+    # get version string from module
+    version = re.search(r"__version__ = ['\"]([^'\"]*)['\"]", f.read(), re.M).group(1)
 
 class NeodroidPackage:
 
@@ -185,4 +190,4 @@ class NeodroidPackage:
 
   @property
   def version(self):
-    return get_version()
+    return version
