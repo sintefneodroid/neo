08bf577b19f60e8b4db251cd0e7adcf572938a61
Christian Heider Nielsen <mrnaah@gmail.com>
2019-05-20 13:53:27 +0200

    0.1.6

--- neodroid/messaging/fbs_state_utilties.py
+++ neodroid/messaging/fbs_state_utilties.py
@@ -1,4 +1,3 @@
-from io import BytesIO
 from typing import Any, Tuple
 
 import imageio
--- neodroid/wrappers/observation_wrapper/observation_wrapper.py
+++ neodroid/wrappers/observation_wrapper/observation_wrapper.py
@@ -57,10 +57,10 @@ class SensorNotAvailableException(Exception):
 
 class CameraObservationWrapper(SingleEnvironmentWrapper):
 
-  def __init__(self, auto_reset=True, image_size=(None, None, 4), **kwargs):
+  def __init__(self, auto_reset=True, **kwargs):
     super().__init__(**kwargs)
     self._auto_reset = auto_reset
-    self._image_size = image_size
+
     self.reset()
 
   def __next__(self):
@@ -71,7 +71,7 @@ class CameraObservationWrapper(SingleEnvironmentWrapper):
   def sensor(self, key):
     if self._last_message:
       state_env_0 = list(self._last_message.values())[0]
-      return extract_camera_observation(state_env_0, key, image_size=self._image_size)
+      return extract_camera_observation(state_env_0, key)
     raise SensorNotAvailableException
 
   def update(self):
@@ -84,5 +84,5 @@ class CameraObservationWrapper(SingleEnvironmentWrapper):
       message = self.fetch_new_frame()
 
     if message:
-      return extract_neodroid_camera(message, image_size=self._image_size)
+      return extract_neodroid_camera(message)
     return None
