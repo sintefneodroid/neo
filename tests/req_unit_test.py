0b8841e888549b6c0c546e91b88ccd305d756cf1
Christian Heider Nielsen <christian.heider@alexandra.dk>
2022-04-12 15:59:02 +0200

    start unreal

--- tests/req_unit_test.py
+++ tests/req_unit_test.py
@@ -12,6 +12,7 @@ _EXTRA_REQUIREMENTS_PATH = Path(__file__).parent.parent/'requirements'
 class TestRequirements(unittest.TestCase):
     """Test availability of required packages."""
 
+
     def test_requirements(self):
         """Test that each required package is available."""
         requirements = pkg_resources.parse_requirements(_REQUIREMENTS_PATH.open())
